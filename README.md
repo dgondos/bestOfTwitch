025e8f4873c31d25b942804f2151e2b20188e6c2
Daniel Gondos <dan.gondos@gmail.com>
2020-10-20 10:52:23 +1100

    recursive method to pull follows

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-Deploy with `npx serverless deploy`
+# BestOfTwitch
 
-Test with `npx serverless invoke -f main -l`
+Gives you a digest of your followed streamers - the digest includes the suggested best timestamp to start watching today's vod (the suggestion is guessed based on the frequency of clips).
+
+# Usage
+
+Implemented as an AWS Lambda function (serverless).
+
+- Change `serverless.yml` to your liking
+- Create a secret in AWS SecretsManager with the key you specify in `serverless.yml` for `TWITCH_CLIENT_SECRET_NAME`. The secret should contain a JSON in this format: `{"CLIENT_ID":"<twitch cliend ID>","CLIENT_SECRET","<twitch client secret>"}`
+- Deploy with `npx serverless deploy`
+- Test with `npx serverless invoke -f main -l -d <event data>`
+
+# Event data
+
+The event data the function is called with should be in this format:
+
+```
+{
+    "twitchUser": "<user to retrieve followers from>"
+}
+```
--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ console.log('Loading function')
 const aws = require('aws-sdk')
 const util = require('util')
 const got = require('got')
+const { stringify } = require('querystring')
 
 exports.handler = async (event, context) => {
     console.log(`Parameters: ${JSON.stringify(event)}`)
 
     await getTwitchClientInfo()
-    .then((clientInfo) => getTwitchToken(clientInfo.CLIENT_ID, clientInfo.CLIENT_SECRET))
-    .then((token) => console.log(`Token: ${token}`))
+    .then((clientInfo) => getTwitchToken(clientInfo.CLIENT_ID, clientInfo.CLIENT_SECRET).then((twitchToken) => [clientInfo, twitchToken]))
+    .then(([clientInfo, twitchToken]) => getFollows(twitchToken, clientInfo.CLIENT_ID, event.twitchUser).then((follows) => [clientInfo, twitchToken, follows]))
+    .then(([clientInfo, twitchToken, follows]) => console.log(JSON.stringify(follows)))
 }
 
 const getTwitchClientInfo = () => {
@@ -34,12 +36,45 @@ const getTwitchClientInfo = () => {
         })
 }
 
+// TODO implement check to only get new token when needed
 const getTwitchToken = (clientId, clientSecret) => {
-    return got.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`).json()
+    return got.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`)
+        .json()
         .then((data) => {
             return data.access_token
         })
         .catch((error) => {
             console.log(`Error when getting token from twitch: ${error}`)
         })
+}
+
+// TODO test, get user ID from user login
+const getFollows = (twitchToken, clientId, user) => {
+    const followsRequest = (cursor, follows) => {
+        let url = `https://api.twitch.tv/helix/users/follows?from_id=${user}&first=100`
+        if (cursor !== undefined) {
+            url = url + `&after=${cursor}`
+        }
+        console.log(url)
+        return got.get(url, {
+            headers: {
+                "Authorization": `Bearer ${twitchToken}`,
+                "Client-Id": clientId
+            }
+        })
+        .json()
+        .then((data) => {
+            if (data.pagination !== undefined && data.pagination.cursor !== undefined) {
+                console.log(`paginating: ${data.pagination.cursor}`)
+                return followsRequest(data.pagination.cursor, follows.concat(data.data))
+            }
+            else {
+                return follows.concat(data.data)
+            }
+        })
+        .catch((error) => {
+            console.log(`Error when getting follows from twitch: ${error}`)
+        })
+    }
+    return followsRequest(undefined, [])
 }
