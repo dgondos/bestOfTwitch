25fa70eae1686ff8cb33ad352e7519ff6fcbedde
Daniel Gondos <dan.gondos@gmail.com>
2020-10-20 09:44:07 +1100

    twitch authorization

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ console.log('Loading function')
 
 const aws = require('aws-sdk')
 const util = require('util')
+const got = require('got')
 
 exports.handler = async (event, context) => {
     console.log(`Parameters: ${JSON.stringify(event)}`)
 
-    console.log(`Client info: ${await getTwitchClientInfo()}`)
+    await getTwitchClientInfo()
+    .then((clientInfo) => getTwitchToken(clientInfo.CLIENT_ID, clientInfo.CLIENT_SECRET))
+    .then((token) => console.log(`Token: ${token}`))
 }
 
 const getTwitchClientInfo = () => {
@@ -19,11 +22,24 @@ const getTwitchClientInfo = () => {
         .then((data) => {
             if ('SecretString' in data) {
                 const secret = data.SecretString
-                return secret
+                return JSON.parse(secret)
             } else {
                 let buff = new Buffer(data.SecretBinary, 'base64');
                 const decodedBinarySecret = buff.toString('ascii');
-                return decodedBinarySecret
+                return JSON.parse(decodedBinarySecret)
             }
+        })
+        .catch((error) => {
+            console.log(`Couldn't retrieve secret: ${error}`)
+        })
+}
+
+const getTwitchToken = (clientId, clientSecret) => {
+    return got.post(`https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`).json()
+        .then((data) => {
+            return data.access_token
+        })
+        .catch((error) => {
+            console.log(`Error when getting token from twitch: ${error}`)
         })
 }
